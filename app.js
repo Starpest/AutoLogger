2f4737d1fef435a38e7d87da5bd4b83fe4d02e46
Raghib Mahatab <98721756+Starpest@users.noreply.github.com>
2023-07-26 20:15:28 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const logger = require('./logger');
+
+// Log some example messages
+logger.error('This is an error message.');
+logger.warn('This is a warning message.');
+logger.info('This is an informational message.');
+logger.debug('This is a debug message.');
